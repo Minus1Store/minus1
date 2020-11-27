e2c8adcc6dda07e67f6fe5d39ac26287e613ab21
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-11-27 10:50:50 +0100

    added status not available if there is no size inserted for product

--- src/components/ProductThumbnail/index.js
+++ src/components/ProductThumbnail/index.js
@@ -7,13 +7,23 @@ const ProductThumbnail = ({image, alt, sizes}) => {
 
     const [hovered, setHovered] = useState(false)
 
+    const checkIfThereAreSizes = () => {
+        return sizes.every(node => node.size.document && node.size.document.data.title)
+    }
+
     return(
         <div className={styles.productThumbnail} onPointerEnter={() => setHovered(true)} onPointerLeave={() => setHovered(false)}>
             <Image fluid={image} alt={alt}/>
             {
-                hovered && sizes && sizes.every(size => size.quantity <= 0) && 
+                hovered && sizes && sizes.every(size => size.quantity <= 0) && checkIfThereAreSizes() &&
                 <div className={styles.soldOutIndicator}>
                     <p>sold out</p>
+                </div>
+            }
+            {
+                hovered && sizes && !checkIfThereAreSizes() &&
+                <div className={styles.soldOutIndicator}>
+                    <p>not available</p>
                 </div>
             }
         </div>
